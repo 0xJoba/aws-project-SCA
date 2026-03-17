be4f2703b72de38421f1ff71c75650ad748e139d
Adedolapo Amunigun <128806648+0xJoba@users.noreply.github.com>
2026-03-17 14:08:38 +0100

    Update comments for clarity in backup.sh

--- backup.sh
+++ backup.sh
@@ -2,7 +2,7 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# 1. Load the variables from the .env file
+# 1. To load the variables from the .env file
 # This is how you avoid hardcoding passwords in the script itself!
 export $(grep -v '^#' .env | xargs)
 
@@ -11,17 +11,17 @@ BACKUP_FILE="backup-$TIMESTAMP.sql"
 
 echo "Creating database backup..."
 
-# 2. Run the dump using the variable we just loaded
+# 2. To run the dump using the variable we just loaded
 docker exec wordpress-db \
 mysqldump -u root -p"$MYSQL_ROOT_PASSWORD" wordpress \
 > "$BACKUP_FILE"
 
 echo "Uploading to S3..."
 
-# 3. Upload to your bucket
+# 3. To upload to your bucket
 aws s3 cp "$BACKUP_FILE" s3://wordpress-backup-joba-2026-904690835815-eu-north-1-an/
 
-# 4. Optional: Remove the local file to save space
+# 4.  To remove the local file to save space
 rm "$BACKUP_FILE"
 
 echo "Backup $BACKUP_FILE uploaded successfully!"
